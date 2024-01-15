53bbb4130b6dde6989fd9c2e665594816bf4d116
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-01-15 21:17:51 +0300

    done

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    function stringReverse(string){
+      return string.split('').reverse().join('');
+  }
+  const input = `robot`
+  const reversedString=stringReverse(input) 
+
+  function reverseChecker(){
+      if(input===reversedString){
+          console.log(true)
+      }
+      else(console.log(false))
+  }
+  reverseChecker()
 }
 
 /* 
   Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
